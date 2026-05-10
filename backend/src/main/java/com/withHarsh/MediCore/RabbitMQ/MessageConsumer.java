701ad0ba7f286a4af49757ff6e866fc12bf5a6ab
Harshvardhan Vathare <harsh1234vathare@gmail.com>
2026-05-10 15:50:55 +0530

    Refactor MessageConsumer to improve logging
    Updated logging and print statements for better clarity.

--- backend/src/main/java/com/withHarsh/MediCore/RabbitMQ/MessageConsumer.java
+++ backend/src/main/java/com/withHarsh/MediCore/RabbitMQ/MessageConsumer.java
@@ -14,19 +14,62 @@ public class MessageConsumer {
         this.emailService = emailService;
     }
 
-    @RabbitListener(queues = {"${rabbitmq.email.queue.name}"})
+    @RabbitListener(queues = "${rabbitmq.email.queue.name}")
     public void consume(AppointmentEmailEventDTO event) {
+
+        System.out.println("CONSUMER STARTED");
+        System.out.println(event);
+
         try {
-            log.info("Received message -> {}", event);
 
             emailService.sendEmail(
                     event.getPatientEmail(),
-                    "🏥 Appointment " + event.getStatus(),
+                    "Appointment " + event.getStatus(),
                     event
             );
 
+            System.out.println("EMAIL SENT SUCCESSFULLY");
+
         } catch (Exception e) {
-            log.error("Error consuming message", e);
+
+            System.out.println("EMAIL FAILED");
+            e.printStackTrace();
         }
     }
 }
+
+
+
+
+// package com.withHarsh.MediCore.RabbitMQ;
+
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.amqp.rabbit.annotation.RabbitListener;
+// import org.springframework.stereotype.Service;
+
+// @Service
+// @Slf4j
+// public class MessageConsumer {
+
+//     private final SmtpEmailService emailService;
+
+//     public MessageConsumer(SmtpEmailService emailService) {
+//         this.emailService = emailService;
+//     }
+
+//     @RabbitListener(queues = {"${rabbitmq.email.queue.name}"})
+//     public void consume(AppointmentEmailEventDTO event) {
+//         try {
+//             log.info("Received message -> {}", event);
+
+//             emailService.sendEmail(
+//                     event.getPatientEmail(),
+//                     "🏥 Appointment " + event.getStatus(),
+//                     event
+//             );
+
+//         } catch (Exception e) {
+//             log.error("Error consuming message", e);
+//         }
+//     }
+// }
